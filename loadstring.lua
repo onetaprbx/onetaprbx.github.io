0acef366c6db90b7f09089a58618d1bb3ab56e86
onetaprbx <exhvh5626@gmail.com>
2026-03-01 14:29:08 +0500

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,17 @@
+--[[
+                      __                                ___.    
+  ____   ____   _____/  |______  ______     ______  __ _\_ |__  
+ /  _ \ /    \_/ __ \   __\__  \ \____ \    \____ \|  |  \ __ \ 
+(  |_| )   |  \  ___/|  |  / __ \|  |_> >   |  |_> >  |  / \_\ \
+ \____/|___|  /\___  >__| (____  /   __/ /\ |   __/|____/|_____/
+            \/     \/          \/|__|    \/ |__|              Art by deportal
+
+
+]]
+
+
+--bruh who said it have logger wtf
+--yeah it have webhooks to collect data about LinoriaLib Errors
+--because we modifed linoria
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/aidarkunakbaev2/Onetap.gg/refs/heads/main/Loader.lua'))();
